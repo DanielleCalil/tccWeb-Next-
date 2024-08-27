4567ade68c2b5538fb165427684f9dc7d0053aec
Daniicalil <159494730+Daniicalil@users.noreply.github.com>
2024-08-27 20:39:59 -0300

    up 27-08

--- src/componentes/cabecalho/index.jsx
+++ src/componentes/cabecalho/index.jsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { IoMenuOutline } from "react-icons/io5";
+import { IoMenuOutline, IoHomeOutline, IoPersonOutline, IoTrailSignOutline, IoStarOutline, IoBookOutline, IoTodayOutline, IoNotificationsOutline, IoPhonePortraitOutline, IoInformationCircleOutline, IoLogOutOutline } from "react-icons/io5";
 
 import styles from './index.module.css';
 import Image from 'next/image';
@@ -45,63 +45,73 @@ export default function Cabecalho() {
 
                 <nav className={styles.menu}>
                     <Link
-                        href="/telaInicial/"
-                        className={rota === "/telaInicial/" ? styles.active : ""}>
-                        <span className="material-symbols-outlined"></span>Início
+                        href="/"
+                        className={rota === "/" ? styles.active : ""}>
+                        <IoHomeOutline className={styles.tpicon} />
+                        Início
                     </Link>
 
                     <Link
-                        href="/perfil/"
-                        className={rota === "/perfil/" ? styles.active : ""}>
-                        <span className="material-symbols-outlined"></span>Perfil
+                        href="/"
+                        className={rota === "/" ? styles.active : ""}>
+                        <IoPersonOutline className={styles.tpicon} />
+                        Perfil
                     </Link>
 
                     <Link
-                        href="/selecao/"
-                        className={rota === "/perfil/" ? styles.active : ""}>
-                        <span className="material-symbols-outlined"></span>Seleção
+                        href="/"
+                        className={rota === "/" ? styles.active : ""}>
+                        <IoTrailSignOutline className={styles.tpicon} />
+                        Seleção
                     </Link>
 
                     <Link
-                        href="/recomendacao/"
-                        className={rota === "/recomendacao/" ? styles.active : ""}>
-                        <span className="material-symbols-outlined"></span>Recomendações
+                        href="/"
+                        className={rota === "/" ? styles.active : ""}>
+                        <IoStarOutline className={styles.tpicon} />
+                        Recomendações
                     </Link>
 
                     <Link
-                        href="/biblioteca/"
-                        className={rota === "/biblioteca/" ? styles.active : ""}>
-                        <span className="material-symbols-outlined"></span>Biblioteca
+                        href="/"
+                        className={rota === "/" ? styles.active : ""}>
+                        <IoBookOutline className={styles.tpicon} />
+                        Biblioteca
                     </Link>
 
                     <Link
-                        href="/reservas/"
-                        className={rota === "/reservas/" ? styles.active : ""}>
-                        <span className="material-symbols-outlined"></span>Reservas
+                        href="/"
+                        className={rota === "/" ? styles.active : ""}>
+                        <IoTodayOutline className={styles.tpicon} />
+                        Reservas
                     </Link>
 
                     <Link
-                        href="/notificacoes/"
-                        className={rota === "/notificacoes/" ? styles.active : ""}>
-                        <span className="material-symbols-outlined"></span>Notificações
+                        href="/"
+                        className={rota === "/" ? styles.active : ""}>
+                        <IoNotificationsOutline className={styles.tpicon} />
+                        Notificações
                     </Link>
 
                     <Link
-                        href="/emprestimos/"
-                        className={rota === "/perfil/" ? styles.active : ""}>
-                        <span className="material-symbols-outlined"></span>Empréstimos
+                        href="/"
+                        className={rota === "/" ? styles.active : ""}>
+                        <IoPhonePortraitOutline className={styles.tpicon} />
+                        Empréstimos
                     </Link>
 
                     <Link
-                        href="/infoContato/"
-                        className={rota === "/infoContato/" ? styles.active : ""}>
-                        <span className="material-symbols-outlined"></span>Informações
+                        href="/"
+                        className={rota === "/" ? styles.active : ""}>
+                        <IoInformationCircleOutline className={styles.tpicon} />
+                        Informações
                     </Link>
 
                     <Link
-                        href="/login/"
-                        className="sair">
-                        <span className="material-symbols-outlined"></span>Sair
+                        href="/"
+                        className={styles.sair}>
+                        <IoLogOutOutline className={styles.tpiconSair} />
+                        Sair
                     </Link>
                     <div className={styles.menuMobile}>
                         <IoMenuOutline onClick={ativaMenu} className={styles.icon} id="logo" />
@@ -117,60 +127,71 @@ export default function Cabecalho() {
                     href='/'
                     onClick={ativaMenu}
                     className={rota === '/' ? styles.active : ''}>
-                    <span className="material-symbols-outlined"></span>Início
+                    <IoHomeOutline className={styles.tpicon} />
+                    Início
                 </Link>
                 <Link
                     href='/'
                     onClick={ativaMenu}
                     className={rota === '/' ? styles.active : ''}>
-                    <span className="material-symbols-outlined"></span>Perfil
+                    <IoPersonOutline className={styles.tpicon} />
+                    Perfil
                 </Link>
                 <Link
                     href='/'
                     onClick={ativaMenu}
                     className={rota === '/' ? styles.active : ''}>
-                    <span className="material-symbols-outlined"></span>Seleção
+                    <IoTrailSignOutline className={styles.tpicon} />
+                    Seleção
                 </Link>
                 <Link
                     href='/'
                     onClick={ativaMenu}
                     className={rota === '/' ? styles.active : ''}>
-                    <span className="material-symbols-outlined"></span>Recomendações
+                    <IoStarOutline className={styles.tpicon} />
+                    Recomendações
                 </Link>
                 <Link
                     href='/'
                     onClick={ativaMenu}
                     className={rota === '/' ? styles.active : ''}>
-                    <span className="material-symbols-outlined"></span>Biblioteca
+                    <IoBookOutline className={styles.tpicon} />
+                    Biblioteca
                 </Link>
                 <Link
                     href='/'
                     onClick={ativaMenu}
                     className={rota === '/' ? styles.active : ''}>
-                    <span className="material-symbols-outlined"></span>Reservas
+                    <IoTodayOutline className={styles.tpicon} />
+                    Reservas
                 </Link>
                 <Link
                     href='/'
                     onClick={ativaMenu}
                     className={rota === '/' ? styles.active : ''}>
-                    <span className="material-symbols-outlined"></span>Notificações
+                    <IoNotificationsOutline className={styles.tpicon} />
+                    Notificações
                 </Link>
                 <Link
                     href='/'
                     onClick={ativaMenu}
                     className={rota === '/' ? styles.active : ''}>
-                    <span className="material-symbols-outlined"></span>Empréstimos
+                    <IoPhonePortraitOutline className={styles.tpicon} />
+                    Empréstimos
                 </Link>
                 <Link
                     href='/'
                     onClick={ativaMenu}
                     className={rota === '/' ? styles.active : ''}>
-                    <span className="material-symbols-outlined"></span>Informações
+                    <IoInformationCircleOutline className={styles.tpicon} />
+                    Informações
                 </Link>
                 <Link
                     href='/'
+                    className={styles.sair}
                     onClick={ativaMenu}>
-                    <span className="material-symbols-outlined"></span>Sair
+                    <IoLogOutOutline className={styles.tpiconSair} />
+                    Sair
                 </Link>
             </div>
         </div >

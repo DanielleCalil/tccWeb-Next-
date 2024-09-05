3284f2adacd5f578b7f173f43e4abdb1415b24dc
Daniicalil <159494730+Daniicalil@users.noreply.github.com>
2024-09-05 15:31:14 -0300

    up 05-09

--- src/app/reservarLivro/page.jsx
+++ src/app/reservarLivro/page.jsx
@@ -1,20 +1,64 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+"use client";
+import React, { useState } from 'react';
+import { DatePicker, Button } from 'antd';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import Link from 'next/link';
+import 'antd/dist/reset.css'; // Importa o CSS do Ant Design
+import styles from './page.module.css'; // Arquivo com os estilos personalizados
+
+const { RangePicker } = DatePicker;
 
 export default function ReservarLivro() {
-    return (
-        <main className={styles.main}>
-            <div className="containerGlobal">
-                <h1 className={styles.informacoes}>Reservar livro</h1>
-                <div className={styles.container}>
-                    <div className={styles.editar}>
-                        <Link href="/perfil/">
-                            <button className={styles.reservButton}>Finalizar reserva</button>
-                        </Link>
-                    </div>
-                </div>
+  const [dates, setDates] = useState([]);
+  const today = new Date();
+
+  const handleDateChange = (values) => {
+    setDates(values || []);
+  };
+
+  return (
+    <main className={styles.main}>
+      <div className="containerGlobal">
+        <h1 className={styles.informacoes}>Reservar livro</h1>
+        <div className={styles.container}>
+          <div className={styles.calendarContainer}>
+            <RangePicker
+              onChange={handleDateChange}
+              format='DD/MM/YYYY'
+              locale={ptBR}
+              disabledDate={(current) => current && current < today}
+            />
+            <div className={styles.datePickers}>
+              <div>
+                <label>Reservar de:</label>
+                <input
+                  type="text"
+                  value={dates[0] ? format(dates[0].toDate(), 'dd/MM/yyyy', { locale: ptBR }) : ''}
+                  readOnly
+                  className={styles.input}
+                />
+              </div>
             </div>
-        </main>
-    );
+            <div className={styles.datePickers}>
+              <div>
+                <label>Até:</label>
+                <input
+                  type="text"
+                  value={dates[1] ? format(dates[1].toDate(), 'dd/MM/yyyy', { locale: ptBR }) : ''}
+                  readOnly
+                  className={styles.input}
+                />
+              </div>
+            </div>
+            <Link href="/perfil/">
+              <Button className={styles.reservButton} type="primary">
+                Finalizar reserva
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }

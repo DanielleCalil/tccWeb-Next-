16d4fee07f348bf6447b6818b4a0162890257bd4
KianSayuri <159494737+KianSayuri@users.noreply.github.com>
2024-10-17 02:19:37 -0300

    17/10

--- src/app/solicitacao/page.jsx
+++ src/app/solicitacao/page.jsx
@@ -1,11 +1,10 @@
-"use client";
+'use client';
+
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
-import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import api from '@/services/api';
-
 import BarraPesquisa from "@/componentes/barraPesquisa/page";
 import ModalConfirmar from '@/componentes/modalConfirmar/page';
 
@@ -22,29 +21,39 @@ const searchOptions = [
 ];
 
 export default function Solicitacao() {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-  const apiPorta = process.env.NEXT_PUBLIC_API_PORTA;
-
-  const imageLoader = ({ src, width, quality }) => {
-    return `${apiUrl}:${apiPorta}${src}?w=${width}&q=${quality || 75}`
-  }
-
-  const [selectedSearchOption, setSelectedSearchOption] = useState('usu_cad');
+  const router = useRouter();
+  const [selectedSearchOption, setSelectedSearchOption] = useState('usu_cod');
   const [selectedUsers, setSelectedUsers] = useState(new Set());
-  const [selectedUsuario, setSelectedUsuario] = useState('Todos');
+  const [usuarioTipo, setUsuarioTipo] = useState("");
   const [showModalConfirm, setShowModalConfirm] = useState(false);
   const [solicitacoesFiltradas, setSolicitacoesFiltradas] = useState([]);
   const [listaUsuarios, setListaUsuarios] = useState([]);
   const [livNome, setlivNome] = useState('');
-  const router = useRouter();
 
   const openModalConfirm = () => setShowModalConfirm(true);
   const closeModalConfirm = () => setShowModalConfirm(false);
 
-  const handleConfirm = () => {
-    console.log("Usuários selecionados:", Array.from(selectedUsers));
-    setShowModalConfirm(false);
-    router.push('../solicitacao');
+  const handleConfirm = async () => {
+    if (selectedUsers.size === 0) {
+      alert("Nenhum usuário selecionado. Por favor, selecione um usuário antes de confirmar.");
+      return;
+    }
+
+    const updatedData = Array.from(selectedUsers).map((usu_cod) => ({
+      usu_cod,
+      usu_tipo: parseInt(usuarioTipo),
+      usu_ativo: 1,
+      usu_aprovado: 1,
+    }));
+
+    try {
+      await api.patch('/analizarUcu', { usuarios: updatedData });
+      setShowModalConfirm(false);
+      router.push('../usuarios/usu_pendentes');
+    } catch (error) {
+      alert("Erro ao atualizar usuários.");
+      console.error(error);
+    }
   };
 
   const toggleUserSelection = (usu_cod) => {
@@ -59,119 +68,75 @@ export default function Solicitacao() {
     });
   };
 
-  function atLivNome(nome) {
-    setlivNome(nome);
-  }
-
   useEffect(() => {
+    async function handleListaUsuarios() {
+      try {
+        const response = await api.post('/usu_pendentes');
+        setListaUsuarios(response.data.dados);
+        setSolicitacoesFiltradas(response.data.dados);
+      } catch (error) {
+        alert('Erro ao buscar usuários pendentes.');
+      }
+    }
     handleListaUsuarios();
   }, []);
 
-  async function handleListaUsuarios() {
-    try {
-      const response = await api.get('/usuarios');
-      setListaUsuarios(response.data.dados);
-    } catch (error) {
-      if (error.response) {
-        alert(error.response.data.mensagem + '\n' + error.response.data.dados);
-      } else {
-        alert('Erro no front-end' + '\n' + error);
-      }
-    }
-  }
-
-  // Função para filtrar as solicitações com base na situação selecionada
   const filtrarSolicitacoes = (situacao) => {
-    const solicitacoesFiltradas = listaUsuarios.filter((solicit) => solicit.situacao === situacao);
-    setSolicitacoesFiltradas(solicitacoesFiltradas);
+    const filtradas = listaUsuarios.filter((solicit) => solicit.situacao === situacao);
+    setSolicitacoesFiltradas(filtradas);
   };
 
   return (
     <main className={styles.main}>
-      <div className="containerGlobal">
-        <h1 className={styles.selecao}>Solicitações de usuários</h1>
-        <BarraPesquisa livNome={livNome} atLivNome={atLivNome} listaLivros={handleListaUsuarios} />
+      <h1 className={styles.selecao}>Solicitações de usuários</h1>
+      <BarraPesquisa livNome={livNome} atLivNome={setlivNome} />
 
-        {/* Radio Buttons para selecionar o critério de pesquisa */}
-        <div className={styles.searchOptions}>
-          {searchOptions.map(option => (
-            <label key={option.value} className={styles.radioLabel}>
-              <input
-                type="radio"
-                name="searchOption"
-                value={option.value}
-                checked={selectedSearchOption === option.value}
-                onChange={() => setSelectedSearchOption(option.value)}
-              />
-              {option.label}
-            </label>
-          ))}
-        </div>
+      {/* Radio Buttons para selecionar o critério de pesquisa */}
+      <div>
+        {searchOptions.map(option => (
+          <label key={option.value}>
+            <input
+              type="radio"
+              name="searchOption"
+              value={option.value}
+              checked={selectedSearchOption === option.value}
+              onChange={() => setSelectedSearchOption(option.value)}
+            />
+            {option.label}
+          </label>
+        ))}
+      </div>
 
-        <div className={styles.situacaoButtons}>
-          {situacao.map(status => (
-            <div
-              className={styles.situacao}
-              key={status}
-              onClick={() => filtrarSolicitacoes(status)}
-            >
-              <Image
-                src={`/solicitacoes/${status.replace(/\s+/g, '_')}.png`}
-                alt={status}
-                width={512}
-                height={512}
-                className={styles.icon}
-              />
-              <p className={styles.textIcon}>{status}</p>
-            </div>
-          ))}
-        </div>
+      <div className={styles.opcao}>
+        <select
+          className={styles.selectInput}
+          value={usuarioTipo}
+          onChange={(e) => setUsuarioTipo(e.target.value)}
+        >
+          <option value="" disabled>Selecione uma opção</option>
+          <option value="2">Funcionário(a) - ADM</option>
+          <option value="1">Professor(a)</option>
+          <option value="0">Aluno(a)</option>
+          <option value="5">Negar acesso</option>
+        </select>
+        <button onClick={openModalConfirm}>Confirmar</button>
+      </div>
 
-        <div className={styles.opcao}>
-          <select id="options" className={styles.selectInput} defaultValue="">
-            <option value="" disabled>
-              Selecione uma opção
-            </option>
-            <option label="Funcionário(a) - ADM" value="2" />
-            <option label="Professor(a)" value="1" />
-            <option label="Aluno(a)" value="0" />
-            <option label="Negar acesso" value="5" />
-          </select>
-          <button
-            type="submit"
-            onClick={openModalConfirm}
-            className={styles.confirmButton}
-          >
-            Confirmar
-          </button>
-        </div>
-
-        {/* Exibe as solicitações filtradas */}
-        <div className={styles.container}>
-          {solicitacoesFiltradas.length > 0 ? (
-            solicitacoesFiltradas.map((solicit) => (
-              <div key={solicit.usu_cod} className={styles.lineSquare}>
-                <div className={styles.inputContainer}>
-                  <p className={styles.info}>Nome: {solicit.usu_nome}</p>
-                  <p className={styles.info}>RM: {solicit.usu_rm}</p>
-                  <p className={styles.info}>E-mail: {solicit.usu_email}</p>
-                  <p className={styles.info}>Curso técnico ou médio: {solicit.cur_nome}</p>
-                  <div className={styles.box}>
-                    <input
-                      type="checkbox"
-                      id={`checkbox-${solicit.usu_cod}`}
-                      checked={selectedUsers.has(solicit.usu_cod)}
-                      onChange={() => toggleUserSelection(solicit.usu_cod)}
-                    />
-                    <label htmlFor={`checkbox-${solicit.usu_cod}`} className={styles.customCheckbox}></label>
-                  </div>
-                </div>
-              </div>
-            ))
-          ) : (
-            <h1>Não há resultados para a requisição</h1>
-          )}
-        </div>
+      {/* Exibe as solicitações filtradas */}
+      <div>
+        {solicitacoesFiltradas.map((solicit) => (
+          <div key={solicit.usu_cod}>
+            <p>Nome: {solicit.usu_nome}</p>
+            <p>RM: {solicit.usu_rm}</p>
+            <p>E-mail: {solicit.usu_email}</p>
+            <p>Curso técnico ou médio: {solicit.cur_nome}</p>
+            <input
+              type="checkbox"
+              checked={selectedUsers.has(solicit.usu_cod)}
+              onChange={() => toggleUserSelection(solicit.usu_cod)}
+            />
+          </div>
+        ))}
       </div>
       <ModalConfirmar
         show={showModalConfirm}
